e4587772bc8a8080ed5f2381f8f7a0666a6d37b2
c0a3bd <40324520+c0a3bd@users.noreply.github.com>
2022-10-27 20:41:57 +0000

    Learning variables

--- main.cs
+++ main.cs
@@ -4,8 +4,11 @@ class Program
 {
     public static void Main(string[] args)
     {
+    int NumberOfVariable = 3;
+      
         // adding comments
         Console.WriteLine("First line");
         Console.WriteLine("Line 2");
+        Console.WriteLine(NumberOfVariable);
     }
 }
